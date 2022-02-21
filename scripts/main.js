147f5840ebeb4a724cf2a72e19777efd80657e57
Jcarbelaez <juan.arbelaez23@gmail.com>
2022-02-21 02:05:49 -0500

    javascript first test

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+debugger
+
+// for (let mes = 1; mes <= 12; mes++) {
+//     if (mes == 10) {
+//         break;
+//     }
+//     console.log('mes', mes, 'del año 2022');
+// }
+
+for (let mes = 1; mes <= 12; mes++) {
+    if (mes == 7) {
+        break;
+    }
+    console.log('mes', mes, 'del año 2022');
+    if (mes == 6)
+    console.log('Es junio, tu cumpleaños');
+
+}
+
+// console.log('mes', mes, 'del año 2022');
