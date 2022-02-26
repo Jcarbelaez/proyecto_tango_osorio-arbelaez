8ef7abf7fc88baeabe3a1c5da6bbbb91103f0598
Juan Camilo Osorio Arbelaez <juan.arbelaez23@gmail.com>
2022-02-26 00:44:40 -0500

    Desafio JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
-debugger
+// debugger
+
+
+
+let cumpleaños = prompt('Ingrese un dia de junio');
+parseInt(cumpleaños);
 
 for (let mes = 1; mes <= 12; mes++) {
     if (mes == 7) {
         break;
     }
     console.log('mes', mes, 'del año 2022');
-    if (mes == 6)
-    console.log('Es junio, tu cumpleaños');
+
+    while (cumpleaños !=23) {
+        cumpleaños = prompt('Intentalo nuevamente');
+    }
+
+    if (mes == 6) {
+        alert('Es ' + cumpleaños + ' de junio, tu cumpleaños.');
+    }
 
 }
