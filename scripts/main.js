8fe5d6efaa36e70a32282ace981736657ef6c022
Juan Camilo Osorio Arbelaez <juan.arbelaez23@gmail.com>
2022-03-12 22:51:39 -0500

    Arrays & methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,17 @@
-// debugger
-
-
-
-// let cumpleaños = prompt('Ingrese un dia de junio');
-// parseInt(cumpleaños);
-
-// for (let mes = 1; mes <= 12; mes++) {
-//     if (mes == 7) {
-//         break;
-//     }
-//     console.log('mes', mes, 'del año 2022');
-
-//     while (cumpleaños !=23) {
-//         cumpleaños = prompt('Intentalo nuevamente');
-//     }
-
-//     if (mes == 6) {
-//         alert('Es ' + cumpleaños + ' de junio, tu cumpleaños.');
-//     }
-
-// }
-debugger
-
-let solicitudVeterinario = prompt("¿Desea solicitar un medico veterinario?");
-
-if ((solicitudVeterinario == "si") || (solicitudVeterinario == "SI")) {
+if (solicitudVeterinario == true) {
     alert("Tu solicitud será verificada.");
-
-    function profesional(nombre, edad, profesion, especialidad) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.profesion = profesion;
-        this.especialidad = especialidad;
-    }
-    const profesional1 = new profesional("Faustino Asprilla", 33, "Veterinario", "Ortopedista");
-    const profesional2 = new profesional("Carlos Valderrama", 35, "Veterinario", "Fisioterapeuta");
-    const profesional3 = new profesional("René Higuita", 32, "Vterinario", "Cirujano");
 
     let seleccioneUnProfesional = parseInt(prompt("Seleccione un profesional 1,2 o 3"));
 
     switch (seleccioneUnProfesional) {
         case 1:
-            alert("El profesional asignado es:" + " " + profesional1.nombre + ". " + profesional1.profesion + ". " + profesional1.especialidad + ".");
+            alert(`El profesional asignado es: ${profesional1.nombre}. ${profesional1.profesion}. ${profesional1.especialidad}`);
             break;
         case 2:
-            alert("El profesional asignado es:" + " " + profesional2.nombre + ". " + profesional2.profesion + ". " + profesional2.especialidad + ".");
+            alert(`El profesional asignado es: ${profesional2.nombre}. ${profesional2.profesion}. ${profesional2.especialidad}`);
             break;
         case 3:
-            alert("El profesional asignado es:" + " " + profesional3.nombre + ". " + profesional3.profesion + ". " + profesional3.especialidad + ".");
+            alert(`El profesional asignado es: ${profesional3.nombre}. ${profesional3.profesion}. ${profesional3.especialidad}`);
             break;
         default:
             alert("El valor ingresado no es válido.");
@@ -57,7 +21,16 @@ if ((solicitudVeterinario == "si") || (solicitudVeterinario == "SI")) {
 } else if (solicitudVeterinario === "") {
     solicitudVeterinario = prompt("Ingresa un valor correcto.");
 }
-while ((solicitudVeterinario != "si") && (solicitudVeterinario != "SI")) {
+while (!solicitudVeterinario) {
     alert("Ok. Hasta una próxima vez.");
     break;
 }
+
+// Filtro de busqueda de mascotas
+
+//Prompt busqueda de mascota
+let ingreseTamaño = prompt("Ingrese busqueda por tamaño de la mascota pequeño, mediano o grande");
+let tamaño = ingreseTamaño.toLowerCase();
+
+let filtroBusquedaMascota = mascotasEnAdopcion.filter(resultado => resultado.tamaño == tamaño);
+console.table(filtroBusquedaMascota);
---
+++ scripts/variables.js
@@ -0,0 +1,84 @@
+//Array de obejtos literales de informacion mascotas en adopción
+const mascotasEnAdopcion = [{
+    id: 1,
+    nombre: "Taxi",
+    sexo: "macho",
+    especie: "Perro",
+    edad: 2,
+    raza: "Criollo",
+    tamaño: "pequeño",
+    esterilizado: "Si"
+},
+{
+    id: 2,
+    nombre: "Tinto",
+    sexo: "macho",
+    especie: "Perro",
+    edad: 1,
+    raza: "Dachshund",
+    tamaño: "pequeño",
+    esterilizado: "No"
+},
+{
+    id: 3,
+    nombre: "Bandido",
+    sexo: "macho",
+    especie: "Perro",
+    edad: 4,
+    raza: "Pug",
+    tamaño: "mediano",
+    esterilizado: "No"
+},
+{
+    id: 4,
+    nombre: "Marta",
+    sexo: "Hembra",
+    especie: "Perro",
+    edad: 2,
+    raza: "Criollo",
+    tamaño: "grande",
+    esterilizado: "Si"
+},
+{
+    id: 5,
+    nombre: "Claudia",
+    sexo: "Hembra",
+    especie: "Perro",
+    edad: 1,
+    raza: "Criollo",
+    tamaño: "grande",
+    esterilizado: "Si"
+},
+{
+    id: 6,
+    nombre: "Parce",
+    sexo: "Macho",
+    especie: "Gato",
+    edad: 7,
+    raza: "Criollo",
+    tamaño: "pequeño",
+    esterilizado: "Si"
+},
+];
+
+//Objeto constructor veterinarios
+function profesional(nombre, edad, profesion, especialidad) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.profesion = profesion;
+    this.especialidad = especialidad;
+}
+
+//Profesionales
+const profesional1 = new profesional("Faustino Asprilla", 33, "Veterinario", "Ortopedista");
+const profesional2 = new profesional("Carlos Valderrama", 35, "Veterinario", "Fisioterapeuta");
+const profesional3 = new profesional("René Higuita", 32, "Veterinario", "Cirujano");
+
+debugger
+
+//Prompt inicial
+let solicitudVeterinario = confirm("¿Desea solicitar un medico veterinario?");
+
+
+
+
